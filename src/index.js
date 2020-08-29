ad596b230f6289a4b5e373b6d4195a662056b97e
kiyoung <popping2606@gmail.com>
2020-08-30 01:11:48 +0900

    react-router-dom : added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode basename={process.env.PUBLIC_URL}>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
