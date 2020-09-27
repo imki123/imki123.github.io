39e6a518a89c214bcf14bad20c6057eace7691ac
kiyoung <popping2606@gmail.com>
2020-09-28 02:28:43 +0900

    index.js: set react-snap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import App from './App';
+import React from 'react'
+import ReactDOM, { hydrate } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
-ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+	hydrate(
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>,
+		rootElement,
+	)
+} else {
+	ReactDOM.render(
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>,
+		document.getElementById('root'),
+	)
+}
