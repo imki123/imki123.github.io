972b54d7aaa901d6118e0bbe391184f7b45839aa
kiyoung <popping2606@gmail.com>
2020-10-05 18:54:34 +0900

    Updates

---
+++ precache-manifest.3d4ac30fdd156f3f964bbf4b45ac9bd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf9cf45712684e43f9c2c7f6d49f81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94e7df20181c97f37b5",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "d3ec094c4fd85726f9f1",
+    "url": "/static/css/main.9dc57f22.chunk.css"
+  },
+  {
+    "revision": "a94e7df20181c97f37b5",
+    "url": "/static/js/2.ab274329.chunk.js"
+  },
+  {
+    "revision": "5426f297221ce9ec659a4ff0a8fc97b2",
+    "url": "/static/js/2.ab274329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ec094c4fd85726f9f1",
+    "url": "/static/js/main.9bd51ed6.chunk.js"
+  },
+  {
+    "revision": "488f4b316c1502937dbb",
+    "url": "/static/js/runtime-main.81f23381.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.530526c363feefc2dd2f3adebaed6102.js"
+  "/precache-manifest.3d4ac30fdd156f3f964bbf4b45ac9bd4.js"
 );
 
 self.addEventListener('message', (event) => {
