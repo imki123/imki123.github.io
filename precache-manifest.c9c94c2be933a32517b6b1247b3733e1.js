b9a8279e36863e0e1b8dc9d1ae89e87857442525
kiyoung <popping2606@gmail.com>
2020-10-12 04:07:02 +0900

    Updates

---
+++ precache-manifest.c9c94c2be933a32517b6b1247b3733e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbaa2ebd507b7dcf95fd872900aeb4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "280a386e478dcf98076a",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "18313773df359da3ae92",
+    "url": "/static/css/main.cfd8a53d.chunk.css"
+  },
+  {
+    "revision": "280a386e478dcf98076a",
+    "url": "/static/js/2.fd75a160.chunk.js"
+  },
+  {
+    "revision": "5426f297221ce9ec659a4ff0a8fc97b2",
+    "url": "/static/js/2.fd75a160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18313773df359da3ae92",
+    "url": "/static/js/main.aee3649f.chunk.js"
+  },
+  {
+    "revision": "488f4b316c1502937dbb",
+    "url": "/static/js/runtime-main.81f23381.js"
+  }
+]);
