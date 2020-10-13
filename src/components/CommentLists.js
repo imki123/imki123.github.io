200d4521d74473de0e774ce25fd9140075d399d1
kiyoung <popping2606@gmail.com>
2020-10-13 09:47:32 +0900

    CommentLists: fix error

--- src/components/CommentLists.js
+++ src/components/CommentLists.js
@@ -5,75 +5,80 @@ import { AppContext } from '../App'
 import './CommentLists.css'
 
 function CommentLists(props) {
-	const store = React.useContext(AppContext)
-	const { list, location } = props
-	const [comments, setComments] = useState()
-	const [commentCnt, setCommentCnt] = useState(10)
-	const [recentAll, setRecentAll] = useState('')
+  const store = React.useContext(AppContext)
+  const { list, location } = props
+  const [comments, setComments] = useState()
+  const [commentCnt, setCommentCnt] = useState(10)
+  const [recentAll, setRecentAll] = useState('')
 
-	useEffect(() => {
-		if (list) setComments(list) //props로 받은 list가 있으면 comments에 저장
+  useEffect(() => {
+    if (list) setComments(list) //props로 받은 list가 있으면 comments에 저장
 
-		if (location && location.pathname === '/comments') {
-			//전체리스트면 Axios로 comments 가져옴
-			setRecentAll('recentAll')
-			let url = process.env.REACT_APP_URL + '/comments/recentAll'
-			//url = process.env.REACT_APP_LOCAL_URL + '/comments/recentAll'
-			Axios.get(url)
-				.then((res) => {
-					//console.log(res.data)
-					setComments(res.data)
-				})
-				.catch((res) => {
-					setComments('')
-					//console.log(res)
-				})
-		}
-	}, [location, list])
+    if (location && (location.pathname === '/comments' || location.pathname === '/comments/')) {
+      //전체리스트면 Axios로 comments 가져옴
+      setRecentAll('recentAll')
+      let url = process.env.REACT_APP_URL + '/comments/recentAll'
+      //url = process.env.REACT_APP_LOCAL_URL + '/comments/recentAll'
+      Axios.get(url)
+        .then((res) => {
+          //console.log('comments:', res.data)
+          setComments(res.data)
+          if (!res.data) {
+            setComments('')
+          }
+        })
+        .catch((res) => {
+          setComments('')
+          //console.log(res)
+        })
+    }
+  }, [location, list])
 
-	useEffect(() => {
-		if(store) store.setReady(false)
-		if (comments) {
-			store.setReady(true)
-		} else if (comments === '') store.setReady(true)
-	})
+  useEffect(() => {
+    if (store) store.setReady(false)
+    if (comments) {
+      store.setReady(true)
+    } else if (comments === '') {
+      store.setReady(true)
+    }
+  })
 
-	//댓글 10개 더보기
-	const more = (e) => {
-		setCommentCnt(commentCnt + 10)
-	}
+  //댓글 10개 더보기
+  const more = (e) => {
+    setCommentCnt(commentCnt + 10)
+  }
 
-	return (
-		<div className={`post recents recentComment ${recentAll}`}>
-			<div className="postListTitle">최신 댓글 {list && <Link to="/comments">더보기</Link>}</div>
-			{comments &&
-				comments.map((i, idx) => (
-					<div key={i.title + idx}>
-						{idx < commentCnt && (
-							<Link to={`/posts/${i.postId}/#comments`}>
-								<div className="postList no-drag">
-									<div className="postListHeader">
-										<div>
-											<span className="postId">{idx + 1}.</span>
-											<span className="username">{i.username}</span>
-										</div>
-										<span className="postDate">{i.publishedDate && i.publishedDate.substring(0, 19).replace('T', ' ')}</span>
-									</div>
-									<div className="postBody">{i.content}</div>
-									<div className="postTitle">{i.title}</div>
-								</div>
-							</Link>
-						)}
-					</div>
-				))}
-			{comments && comments.length > commentCnt && (
-				<div className="more">
-					<span className="moreButton" onClick={more}>
-						댓글 더보기
-					</span>
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div className={`post recents recentComment ${recentAll}`}>
+      <div className="postListTitle">최신 댓글 {list && <Link to="/comments">더보기</Link>}</div>
+      {comments &&
+        comments.map((i, idx) => (
+          <div key={i.title + idx}>
+            {idx < commentCnt && (
+              <Link to={`/posts/${i.postId}/#comments`}>
+                <div className="postList no-drag">
+                  <div className="postListHeader">
+                    <div>
+                      <span className="postId">{idx + 1}.</span>
+                      <span className="username">{i.username}</span>
+                    </div>
+                    <span className="postDate">{i.publishedDate && i.publishedDate.substring(0, 19).replace('T', ' ')}</span>
+                  </div>
+                  <div className="postBody">{i.content}</div>
+                  <div className="postTitle">{i.title}</div>
+                </div>
+              </Link>
+            )}
+          </div>
+        ))}
+      {comments && comments.length > commentCnt && (
+        <div className="more">
+          <span className="moreButton" onClick={more}>
+            댓글 더보기
+          </span>
+        </div>
+      )}
+    </div>
+  )
 }
 export default React.memo(CommentLists)
