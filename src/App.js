516e77ab6bcddc0737831c0a7de864c72c54094e
kiyoung <popping2606@gmail.com>
2020-11-06 15:53:11 +0900

    App: fix description

--- src/App.js
+++ src/App.js
@@ -152,6 +152,7 @@ function App() {
 				<Meta
 					data={{
 						title: '임기의 코딩 블로그',
+						description: '주로 Javascript, React, ReactNative, Node 관련 글을 포스팅합니다. 감사합니다. 😄',
 						image: process.env.PUBLIC_URL + '/images/imcat_800x400.png',
 					}}
 				/>
